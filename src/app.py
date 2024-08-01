7aca8cadd6dbebb1570f5e2d22bdf60ba069835d
Satoshi Sumura <satoshi_sumura@i-guazu.co.jp>
2024-08-01 17:56:19 +0900

    first commit

---
+++ src/app.py
@@ -0,0 +1,13 @@
+from flask import Flask,render_template
+import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    dt_now = datetime.datetime.now()
+    return render_template('index.html', now=dt_now.strftime('%Y年%m月%d日 %H:%M:%S'))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
